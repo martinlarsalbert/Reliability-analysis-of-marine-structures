a274845b08ec1658fdf9b901365c993f7e36a498
Martin Alexandersson <marale@kth.se>
2020-04-23 15:47:54 +0200

    Finnished the mandatory part of this assignment.

--- assignments/assignment_1_problem_2.m
+++ assignments/assignment_1_problem_2.m
@@ -1,7 +1,7 @@
 close all;
 clear all;
 clc;
-addpath('../../wafo_old')
+addpath('../wafo_old')
 initwafo
 
 %%
@@ -14,7 +14,7 @@ data = Virk(1:136,:);
 N = data(:,8);  % speciment_7
 A = data(:,1);
 
-figure();
+figure(1);
 plot(A, N);
 xlabel('A (Crack length)');
 ylabel('N (Number of oscillations)');
@@ -25,7 +25,7 @@ dNdA = gradient(N,A);
 
 
 
-figure();
+figure(2);
 plot(A, dNdA);
 xlabel('A (Crack length)');
 ylabel('dN/dA');
@@ -46,12 +46,26 @@ X=[ones(n,1),x_1];
 coeffs = regress(y,X);
 c_0=coeffs(1);
 c_1=coeffs(2);
+y_regress =X*coeffs;
+
+figure(3);
+plot(x_1,y);
+hold on;
+correlation_y = corrcoef([x_1,y]);
+title(['Correlation coefficient: ', num2str(correlation_y(1,end),'%0.2f')]);
+
+plot(x_1,y_regress);
+legend_regress = ['regression (c_0:',num2str(c_0,'%0.4f'), 'c_1:' num2str(c_1,'%0.4f'),')'];
+legend('Numerical derivation',legend_regress);
+xlabel('ln(A)');
+ylabel('ln(dN/dA)');
 
 beta = exp(c_0);
 alpha = c_1;
 dNdA_regress = beta.*A.^alpha;
+figure(2);
 plot(A, dNdA_regress);
-legend_regress = ['regress \alpha:',num2str(alpha,'%0.4f'), '\beta:' num2str(beta,'%0.0f')];
+legend_regress = ['regression (\alpha:',num2str(alpha,'%0.4f'), '\beta:' num2str(beta,'%0.0f'),')'];
 legend('Numerical derivation',legend_regress);
 
 % Manual Least square fit:
@@ -72,16 +86,83 @@ c_0_ = (S_y-c_1_*S_x)/n;
 % Hypotesis: alpha=0
 % --> c_1=0
 % --> y = c_0
-y_estimation = c_0;
-error_estimation = y - y_estimation;
-X=error_estimation;
-SSR=sum((error_estimation-mean(error_estimation)).^2);
-SSE=sum((y_estimation-error_estimation).^2);
-SST=sum((y_estimation-mean(y_estimation)).^2);
-% you could check if SST=SSR+SSE
-F_ratio=(SSR/1)/(SSE/7);
-P=1-fcdf(x_1, 1,7);
 
+xest = y_regress;
+
+SSR=sum((xest-mean(y)).^2);
+SSE=sum((y-xest).^2);
+SST=sum((y-mean(y)).^2);
+% you could check if SST=SSR+SSE
+if ((SST-(SSR+SSE))/(SSR+SSE)>0.01)
+    warning('SST=SSR+SSE Failed');
+end
+
+MSR = SSR/1;
+MSE = SSE/(n-2);
+
+F_ratio=(MSR)/(MSE);
+P=1-fcdf(F_ratio, 1,n-2);
+
+if F_ratio > P
+    disp('alpha=0 Rejected!!!');
+else
+    disp('alpha=0 Accepted');
+end
+
+%%
+% Regression diagnostics (check the residuals)
+
+epsilon = y - y_regress;
+
+% Check the normal distribution
+figure()
+normplot(epsilon);
+
+% Check the constant variance
+figure()
+plot(y,epsilon, 'bo');
+hold on;
+xlabel('y=ln(dN/dA)');
+ylabel('Residual error \epsilon');
+correlation_y_error = corrcoef([y,epsilon]);
+title(['Correlation coefficient: ', num2str(correlation_y_error(1,end),'%0.2f')]);
+X2=[ones(n,1),y];
+coeffs2 = regress(epsilon,X2);
+epsilon_regress_y=X2*coeffs2;
+plot(y,epsilon_regress_y, 'k:');
+legend('\epsilon','error trend');
+
+
+% Check the correlation between the residuals and independent variables
+figure()
+plot(x_1,epsilon,'r*');
+hold on;
+xlabel('x_1=ln(A)');
+ylabel('Residual error \epsilon');
+correlation_x_error = corrcoef([x_1,epsilon]);
+title(['Correlation coefficient: ', num2str(correlation_x_error(1,end),'%0.2f')]);
+X3=[ones(n,1),x_1];
+coeffs3 = regress(epsilon,X3);
+epsilon_regress_x_1=X2*coeffs3;
+plot(x_1,epsilon_regress_x_1, 'k:');
+legend('\epsilon','error trend');
+
+n_parts = 2;
+part_length = floor(length(epsilon)/n_parts);
+figure();
+hold on;
+legends = {}
+for nn=1:(n_parts)
+    index_1 = 1+(nn-1)*part_length;
+    index_2 = nn*part_length;
+    x_= x_1(index_1:index_2);
+    y_= epsilon(index_1:index_2);
+    plot(x_, y_,'.');    
+    legends{end+1} = ['var(\epsilon)=',num2str(var(y_))];
+end
+legend(legends);
+xlabel('x_1=ln(A)');
+ylabel('Residual error \epsilon');
 
 
 
